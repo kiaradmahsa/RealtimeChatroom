96e40f73babaa10336ad0ed5341316274fca4fe8
mahsa <kiaradmahsa@gmail.com>
2024-01-18 12:44:59 +0000

    update rooms

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,8 @@ const chatList = document.querySelector('.chat-list');
 const newChatForm = document.querySelector('.new-chat');
 const newNameForm = document.querySelector('.new-name');
 const updateMssg = document.querySelector('.update-mssg');
+const rooms = document.querySelector('.chat-rooms');
+
 //add a new chat
 newChatForm.addEventListener('submit', e=>{
     e.preventDefault();
@@ -27,8 +29,20 @@ newNameForm.addEventListener('submit', e =>{
     updateMssg.innerText =  `your name was updated to ${newName}`;
     setTimeout(()=> updateMssg.innerText = "" , 3000);
 });
+
+//update the chatroom
+
+rooms.addEventListener('click', e =>{
+    if(e.target.tagName === 'BUTTON'){
+        chatUI.clear();
+        chatroom.updateRoom(e.target.getAttribute('id'));
+        chatroom.getChats(chat => chatUI.render(chat));
+    }
+});
+
 //check local storage for name
 const username = localStorage.username ? localStorage.username : 'Anonymous';
+
 //class instances
 const chatUI = new ChatUI(chatList);
 const chatroom = new Chatroom('general',username);
--- scripts/ui.js
+++ scripts/ui.js
@@ -3,12 +3,16 @@ class ChatUI {
         this.list = list;
     }
 
+    clear(){
+        this.list.innerHTML = '';
+    }
+
     render(data){
         const when = dateFns.distanceInWordsToNow(
             data.created_at.toDate(),
             { addSuffix:true }
           );
-          
+
         const html = `
         <li class="list-group-item">
             <span class="username">${data.username}</span>
